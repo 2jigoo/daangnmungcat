89f54bdd5aa65930579b2a1ce7cd3620b8c0288b
hjkim <hjkimxoxo@gmail.com>
2020-12-17 22:22:51 +0900

    contextDataSourceTest, contectSqlSessionTest 완

--- src/main/java/daangnmungcat/dto/Member.java
+++ src/main/java/daangnmungcat/dto/Member.java
@@ -16,6 +16,7 @@ public class Member {
 	private String profileText;
 	private LocalDateTime regdate;
 	
+	
 	/*
 	private boolean identifyYn; // 본인인증 여부
 	private LocalDate birthday;
@@ -24,4 +25,111 @@ public class Member {
 	private String address2;
 	priavate int mileage;
 	*/
+	
+	public Member() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getPwd() {
+		return pwd;
+	}
+
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getNickname() {
+		return nickname;
+	}
+
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	public String getProfilePic() {
+		return profilePic;
+	}
+
+
+	public void setProfilePic(String profilePic) {
+		this.profilePic = profilePic;
+	}
+
+
+	public String getProfileText() {
+		return profileText;
+	}
+
+
+	public void setProfileText(String profileText) {
+		this.profileText = profileText;
+	}
+
+
+	public LocalDateTime getRegdate() {
+		return regdate;
+	}
+
+
+	public void setRegdate(LocalDateTime regdate) {
+		this.regdate = regdate;
+	}
+
+
+	@Override
+	public String toString() {
+		return String.format(
+				"Member [id=%s, pwd=%s, name=%s, nickname=%s, email=%s, phone=%s, profilePic=%s, profileText=%s, regdate=%s]",
+				id, pwd, name, nickname, email, phone, profilePic, profileText, regdate);
+	}
+	
+	
+	
+	
 }
