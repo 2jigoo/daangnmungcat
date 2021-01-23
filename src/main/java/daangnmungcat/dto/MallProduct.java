e4c401034189f85a5b15375f32cae4279a87d075
osj <1996otnwjd@gmail.com>
2021-01-23 21:57:32 +0900

    배송 수정

--- src/main/java/daangnmungcat/dto/MallProduct.java
+++ src/main/java/daangnmungcat/dto/MallProduct.java
@@ -33,7 +33,7 @@ public class MallProduct {
 	private String image1;
 	private String image2;
 	private String image3;
-	private int deliveryKind;
+	private String deliveryKind;
 	private int deliveryCondition;
 	private int deliveryPrice;
 	
